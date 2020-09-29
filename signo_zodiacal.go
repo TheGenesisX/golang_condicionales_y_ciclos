a60341da09ce02abebcbfbf1de00f29bdee5673b
Alejandro Silva <jose.silva5468@alumnos.udg.mx>
2020-09-29 12:02:55 -0500

    Cambio de nombres de los signos zodiacales por minusculas

--- signo_zodiacal.go
+++ signo_zodiacal.go
@@ -12,75 +12,75 @@ func main() {
 	switch mes {
 	case 1:
 		if dia >= 1 && dia <= 20 {
-			fmt.Print("Capricornio")
+			fmt.Print("capricornio")
 		} else if dia >= 21 && dia <= 31 {
-			fmt.Print("Acuario")
+			fmt.Print("acuario")
 		}
 	case 2:
 		if dia >= 1 && dia <= 18 {
-			fmt.Print("Acuario")
+			fmt.Print("acuario")
 		} else if dia >= 19 && dia <= 29 {
-			fmt.Print("Piscis")
+			fmt.Print("piscis")
 		}
 	case 3:
 		if dia >= 1 && dia <= 20 {
-			fmt.Print("Piscis")
+			fmt.Print("piscis")
 		} else if dia >= 21 && dia <= 31 {
-			fmt.Print("Aries")
+			fmt.Print("aries")
 		}
 	case 4:
 		if dia >= 1 && dia <= 20 {
-			fmt.Print("Aries")
+			fmt.Print("aries")
 		} else if dia >= 21 && dia <= 30 {
-			fmt.Print("Tauro")
+			fmt.Print("tauro")
 		}
 	case 5:
 		if dia >= 1 && dia <= 20 {
-			fmt.Print("Tauro")
+			fmt.Print("tauro")
 		} else if dia >= 21 && dia <= 31 {
-			fmt.Print("Geminis")
+			fmt.Print("geminis")
 		}
 	case 6:
 		if dia >= 1 && dia <= 21 {
-			fmt.Print("Geminis")
+			fmt.Print("geminis")
 		} else if dia >= 22 && dia <= 30 {
-			fmt.Print("Cancer")
+			fmt.Print("cancer")
 		}
 	case 7:
 		if dia >= 1 && dia <= 22 {
-			fmt.Print("Cancer")
+			fmt.Print("cancer")
 		} else if dia >= 23 && dia <= 31 {
-			fmt.Print("Leo")
+			fmt.Print("leo")
 		}
 	case 8:
 		if dia >= 1 && dia <= 22 {
-			fmt.Print("Leo")
+			fmt.Print("leo")
 		} else if dia >= 23 && dia <= 31 {
-			fmt.Print("Virgo")
+			fmt.Print("virgo")
 		}
 	case 9:
 		if dia >= 1 && dia <= 22 {
-			fmt.Print("Virgo")
+			fmt.Print("virgo")
 		} else if dia >= 23 && dia <= 30 {
-			fmt.Print("Libra")
+			fmt.Print("libra")
 		}
 	case 10:
 		if dia >= 1 && dia <= 22 {
-			fmt.Print("Libra")
+			fmt.Print("libra")
 		} else if dia >= 23 && dia <= 31 {
-			fmt.Print("Escorpio")
+			fmt.Print("escorpio")
 		}
 	case 11:
 		if dia >= 1 && dia <= 22 {
-			fmt.Print("Escorpio")
+			fmt.Print("escorpio")
 		} else if dia >= 23 && dia <= 30 {
-			fmt.Print("Sagitario")
+			fmt.Print("sagitario")
 		}
 	case 12:
 		if dia >= 1 && dia <= 21 {
-			fmt.Print("Sagitario")
+			fmt.Print("sagitario")
 		} else if dia >= 22 && dia <= 31 {
-			fmt.Print("Capricornio")
+			fmt.Print("capricornio")
 		}
 	}
 }
